6f348df164a9404ac530917b38ef4c9e7fe80150
VishalPachpor <Vishalpatil080502@gmail.com>
2023-11-13 18:02:29 +0530

    Organize the feed information into distinct sections for improved clarity.

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -1,5 +1,5 @@
-import React, { forwardRef } from 'react'
-import "./Post.css"
+import React, { forwardRef } from "react";
+import "./Post.css";
 import { Avatar } from "@material-ui/core";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser";
 import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
@@ -7,41 +7,51 @@ import RepeatIcon from "@material-ui/icons/Repeat";
 import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 import PublishIcon from "@material-ui/icons/Publish";
 
-const Post = forwardRef(({
-    displayName,
-    username,
-    verified,
-    text,
-    image,
-    avatar,
-}, ref) => {
-    return <div className="post" ref={ref}>
+const Post = forwardRef(
+  ({ displayName, username, verified, text, image, avatar }, ref) => {
+    return (
+      <div className="post" ref={ref}>
+        {/* Avatar */}
         <div className="post__avatar">
-            <Avatar src={avatar} />
+          <Avatar src={avatar} />
         </div>
-        <div className="post__body">
-            <div className="post__header">
-                <div className="post__headerText">
-                    <h3>
-                        {displayName}{" "}
-                        <span className="post__headerSpecial">
-                            {verified && <VerifiedUserIcon className="post__badge" />}@{username}
-                        </span>
-                    </h3>
-                </div>
-                <div className="post__headerDescription">
-                    <p>{text}</p>
-                </div>
-            </div>
-            <img src={image} alt="" />
-            <div className="post__footer">
-                <ChatBubbleOutlineIcon fontSize="small" />
-                <RepeatIcon fontSize="small" />
-                <FavoriteBorderIcon fontSize="small" />
-                <PublishIcon fontSize="small" />
-            </div>
-        </div>
-    </div>;
-})
 
-export default Post
+        {/* Post Body */}
+        <div className="post__body">
+          {/* Post Header */}
+          <div className="post__header">
+            {/* Header Text */}
+            <div className="post__headerText">
+              <h3>
+                {displayName}{" "}
+                <span className="post__headerSpecial">
+                  {verified && <VerifiedUserIcon className="post__badge" />}@
+                  {username}
+                </span>
+              </h3>
+            </div>
+
+            {/* Header Description */}
+            <div className="post__headerDescription">
+              <p>{text}</p>
+            </div>
+          </div>
+
+          {/* Post Image */}
+          <img src={image} alt="" />
+
+          {/* Post Footer */}
+          <div className="post__footer">
+            {/* Icons for Interaction */}
+            <ChatBubbleOutlineIcon fontSize="small" />
+            <RepeatIcon fontSize="small" />
+            <FavoriteBorderIcon fontSize="small" />
+            <PublishIcon fontSize="small" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+);
+
+export default Post;
--- src/Components/ResultSearch.js
+++ src/Components/ResultSearch.js
@@ -1,47 +1,66 @@
- import "./lens.css"; // uncomment this and make changes also
+import React from "react";
+import "./lens.css"; // Uncomment this line if required
 
 function SearchNextResult({ singleSearchFeed, authorData, authorUrii }) {
-    return (
-        <div className="feeds">
-            <div className="feedImg">
-                <div>
-                    <img src={authorUrii} alt="person face" />
-                    <p>handle: {authorData.name}</p>
-                </div>
-            </div>
-            <div className="network">
-                <p>Network: {singleSearchFeed.network}</p>
-            </div>
-            <div className="engagement">
-                <p>Platform: {singleSearchFeed.platform}</p>
-            </div>
-            <div className="engagement">
-                <p>Engagement Type: {singleSearchFeed.type}</p>
-            </div>
-            <div className="from">
-                <p>from: {singleSearchFeed.address_from}</p>
-            </div>
-            <div className="to">
-                <p>to: {singleSearchFeed.address_to}</p>
-            </div>
-            <div className="to">
-                <p>
-                    comment:{" "}
-                    {singleSearchFeed.actions &&
-                        singleSearchFeed.actions[0].metadata.body}
-                </p>
-            </div>
-            <div className="flink">
-                <p>
-                    Full view:{" "}
-                    {singleSearchFeed.actions &&
-                        singleSearchFeed.actions[0].related_urls[0]}
-                </p>
-            </div>
-            <div className="time">
-                <p>Time: {singleSearchFeed.created_at}</p>
-            </div>
+  return (
+    <div className="feeds">
+      {/* Feed Image */}
+      <div className="feedImg">
+        <div>
+          <img src={authorUrii} alt="person face" />
+          <p>handle: {authorData.name}</p>
         </div>
-    );
+      </div>
+
+      {/* Network Information */}
+      <div className="network">
+        <p>Network: {singleSearchFeed.network}</p>
+      </div>
+
+      {/* Engagement Platform */}
+      <div className="engagement">
+        <p>Platform: {singleSearchFeed.platform}</p>
+      </div>
+
+      {/* Engagement Type */}
+      <div className="engagement">
+        <p>Engagement Type: {singleSearchFeed.type}</p>
+      </div>
+
+      {/* Sender Information */}
+      <div className="from">
+        <p>from: {singleSearchFeed.address_from}</p>
+      </div>
+
+      {/* Receiver Information */}
+      <div className="to">
+        <p>to: {singleSearchFeed.address_to}</p>
+      </div>
+
+      {/* Comment Section */}
+      <div className="to">
+        <p>
+          comment:{" "}
+          {singleSearchFeed.actions &&
+            singleSearchFeed.actions[0].metadata.body}
+        </p>
+      </div>
+
+      {/* Full View Link */}
+      <div className="flink">
+        <p>
+          Full view:{" "}
+          {singleSearchFeed.actions &&
+            singleSearchFeed.actions[0].related_urls[0]}
+        </p>
+      </div>
+
+      {/* Timestamp */}
+      <div className="time">
+        <p>Time: {singleSearchFeed.created_at}</p>
+      </div>
+    </div>
+  );
 }
+
 export default SearchNextResult;
